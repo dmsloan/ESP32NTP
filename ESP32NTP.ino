9f471ca407706879cf8ff9d08aa99268a64e85ba
Derek's Office <dsloan@ibe-inc.com>
2018-09-07 16:39:58 -0700

    Initial commit

---
+++ ESP32NTP.ino
@@ -0,0 +1,43 @@
+
+#include <time.h> 
+#include <WiFi.h>
+#include "WiFiInfo.h"
+
+
+const char* ssid     = SSID;
+const char* password = PASSWORD;
+
+long timezone = -8; 
+byte daysavetime = 1;
+
+
+void setup(){
+    Serial.begin(115200);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("Contacting Time Server");
+	configTime(3600*timezone, daysavetime*3600, "time.nist.gov", "0.pool.ntp.org", "1.pool.ntp.org");
+	struct tm tmstruct ;
+    delay(2000);
+    tmstruct.tm_year = 0;
+    getLocalTime(&tmstruct, 5000);
+	Serial.printf("\nNow is : %d-%02d-%02d %02d:%02d:%02d\n",(tmstruct.tm_year)+1900,( tmstruct.tm_mon)+1, tmstruct.tm_mday,tmstruct.tm_hour , tmstruct.tm_min, tmstruct.tm_sec);
+    Serial.println("");
+}
+
+void loop(){
+
+}
---
+++ WiFiInfoExample.h
@@ -0,0 +1,5 @@
+// this is an example WiFiInfo.h file. Change the name to WiFiInfo.h and replace the 
+// XXXXXXX
+
+#define SSID "XXXXXXXXXXX" // if your key is a string it needs to be in quotes. Note no semicolon after a hash define
+#define PASSWORD "XXXXXXXXXXX"
